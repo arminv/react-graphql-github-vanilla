d328e8d1d80e555c198cfab3d4e3da9585194bc6
arminv <arminv@users.noreply.github.com>
2020-08-16 01:12:40 -0400

    End of page 75 (pagination in React)

--- src/App.js
+++ src/App.js
@@ -51,37 +51,80 @@ const axiosGitHubGraphQL = axios.create({
 // `;
 
 const GET_ISSUES_OF_REPOSITORY = `
-query ($organization: String!, $repository: String!) {
+  query ($organization: String!, $repository: String!, $cursor: String) {
     organization(login: $organization) {
-name
-url
-repository(name: $repository) {
+      name
+      url
+      repository(name: $repository) {
         name
         url
-        issues(last: 5) {
-edges { node {
-id
-title
-url }
-} }
-} }
-} `;
+        issues(first: 5, after: $cursor, states: [OPEN]) {
+          edges {
+            node {
+              id
+              title
+              url
+              reactions(last: 3) {
+                edges {
+                  node {
+                    id
+                    content
+                  }
+                }
+              }
+            }
+          }
+          totalCount
+          pageInfo {
+            endCursor
+            hasNextPage
+          }
+        }
+      }
+    }
+  }
+`;
 
-const getIssuesOfRepository = (path) => {
+const getIssuesOfRepository = (path, cursor) => {
   const [organization, repository] = path.split('/');
 
   return axiosGitHubGraphQL.post('', {
-    // query: getIssuesOfRepositoryQuery(organization, repository),
     query: GET_ISSUES_OF_REPOSITORY,
-    variables: { organization, repository },
+    variables: { organization, repository, cursor },
   });
 };
 
 // Note: this is a higher-order function (it has to be for useState() to work properly):
-const resolveIssuesQuery = (queryResult) => () => ({
-  organization: queryResult.data.data.organization,
-  errors: queryResult.data.errors,
-});
+const resolveIssuesQuery = (queryResult, cursor) => (state) => {
+  const { data, errors } = queryResult.data;
+
+  // Note: this means we are only fetching the first page (e.g. when page first mounts/loads, etc.):
+  if (!cursor) {
+    return {
+      organization: data.organization,
+      errors,
+    };
+  }
+
+  const { edges: oldIssues } = state.organization.repository.issues;
+  const { edges: newIssues } = data.organization.repository.issues;
+  const updatedIssues = [...oldIssues, ...newIssues];
+
+  return {
+    organization: {
+      ...data.organization,
+      repository: {
+        ...data.organization.repository,
+        issues: {
+          ...data.organization.repository.issues,
+          // Note: only the edges property should be updated with the merged list of issues:
+          edges: updatedIssues,
+        },
+      },
+    },
+    errors,
+  };
+};
 
 const TITLE = 'React GraphQL GitHub Client';
 class App extends Component {
@@ -90,23 +133,32 @@ class App extends Component {
     organization: null,
     errors: null,
   };
+
   componentDidMount() {
     // fetch data
     this.onFetchFromGitHub(this.state.path);
   }
+
   onChange = (event) => {
     this.setState({ path: event.target.value });
   };
+
   onSubmit = (event) => {
     // fetch data
     this.onFetchFromGitHub(this.state.path);
     event.preventDefault();
   };
 
-  onFetchFromGitHub = (path) => {
-    getIssuesOfRepository(path).then((queryResult) =>
-      this.setState(resolveIssuesQuery(queryResult))
+  onFetchFromGitHub = (path, cursor) => {
+    getIssuesOfRepository(path, cursor).then((queryResult) =>
+      this.setState(resolveIssuesQuery(queryResult, cursor))
     );
+  };
+
+  onFetchMoreIssues = () => {
+    const { endCursor } = this.state.organization.repository.issues.pageInfo;
+
+    this.onFetchFromGitHub(this.state.path, endCursor);
   };
 
   render() {
@@ -115,8 +167,9 @@ class App extends Component {
     return (
       <div>
         <h1>{TITLE}</h1>
+
         <form onSubmit={this.onSubmit}>
-          <label htmlFor='url'>Show open issues for https://github.com/ </label>
+          <label htmlFor='url'>Show open issues for https://github.com/</label>
           <input
             id='url'
             type='text'
@@ -126,9 +179,15 @@ class App extends Component {
           />
           <button type='submit'>Search</button>
         </form>
+
         <hr />
+
         {organization ? (
-          <Organization organization={organization} errors={errors} />
+          <Organization
+            organization={organization}
+            errors={errors}
+            onFetchMoreIssues={this.onFetchMoreIssues}
+          />
         ) : (
           <p>No information yet ...</p>
         )}
@@ -137,7 +196,7 @@ class App extends Component {
   }
 }
 
-const Organization = ({ organization, errors }) => {
+const Organization = ({ organization, errors, onFetchMoreIssues }) => {
   if (errors) {
     return (
       <p>
@@ -152,12 +211,15 @@ const Organization = ({ organization, errors }) => {
         <strong>Issues from Organization:</strong>
         <a href={organization.url}>{organization.name}</a>
       </p>
-      <Repository repository={organization.repository} />
+      <Repository
+        repository={organization.repository}
+        onFetchMoreIssues={onFetchMoreIssues}
+      />
     </div>
   );
 };
 
-const Repository = ({ repository }) => (
+const Repository = ({ repository, onFetchMoreIssues }) => (
   <div>
     <p>
       <strong>In Repository:</strong>
@@ -168,9 +230,21 @@ const Repository = ({ repository }) => (
       {repository.issues.edges.map((issue) => (
         <li key={issue.node.id}>
           <a href={issue.node.url}>{issue.node.title}</a>
+
+          <ul>
+            {issue.node.reactions.edges.map((reaction) => (
+              <li key={reaction.node.id}>{reaction.node.content}</li>
+            ))}
+          </ul>
         </li>
       ))}
     </ul>
+
+    <hr />
+
+    {repository.issues.pageInfo.hasNextPage && (
+      <button onClick={onFetchMoreIssues}>More</button>
+    )}
   </div>
 );
 
